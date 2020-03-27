d3d46a16fc3cd7fd4a6e02201818ac56e436f976
Yevgeniy Alishevets <yevgalis@protonmail.com>
2020-03-27 23:32:46 +0600

    basic config for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
         },
