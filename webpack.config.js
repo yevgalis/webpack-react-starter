6b3c76daea13142d0c3c3dca5f5e40f762d18ea4
Yevgeniy Alishevets <yevgalis@protonmail.com>
2021-02-07 14:00:01 +0600

    updated postcss and postcss-flexbugs-fixes

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader'
         }
