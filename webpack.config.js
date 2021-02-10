86623600abe5f0689462bd83a19b301f7abd4750
Yevgeniy Alishevets <yevgalis@protonmail.com>
2021-02-10 21:33:59 +0600

    changed terser plugin settings

--- webpack.config.js
+++ webpack.config.js
@@ -42,27 +42,9 @@ module.exports = {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
-          parse: {
-            ecma: 8
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            comparisons: false,
-            inline: 2
-          },
-          mangle: {
-            safari10: true
-          },
-          keep_classnames: isProduction,
-          keep_fnames: isProduction,
-          output: {
-            ecma: 5,
-            comments: false,
-            ascii_only: true
-          },
-          extractComments: false
-        }
+          output: { comments: false }
+        },
+        extractComments: false
       })
     ]
   },
