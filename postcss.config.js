be33d3fddfd364abe7d3171373bfe3edba6f9883
Yevgeniy Alishevets <yevgalis@protonmail.com>
2021-02-25 20:48:14 +0600

    added postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: 'postcss-safe-parser',
   plugins: {
+    'postcss-import': {},
     'postcss-flexbugs-fixes': {},
     'postcss-preset-env': { stage: 3 },
     'cssnano': {}
